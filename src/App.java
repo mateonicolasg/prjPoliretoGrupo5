42f4200fe223670d4f5b1f8128968f3af8f4d07c
Francisco Torres <francisco.torres@epn.edu.ec>
2023-12-05 12:57:30 -0500

    new exs

--- src/App.java
+++ src/App.java
@@ -5,36 +5,36 @@ public class App {
         Scanner sc = new Scanner(System.in);
         MateoGrupo5 oSerieNumeros12 = new MateoGrupo5();
         SerieTercerEjercicio SerieTercerEjercicio = new SerieTercerEjercicio();
-
+        SerieCaracteres9 serie9=new SerieCaracteres9();
         System.out.println("GRUPO: ");        
         System.out.println("INTEGRANTES: Simbaña.Mateo, Vera.Edison, Negrete.Francisco, Unaucho.David, Velasco.Matheus, Santacruz.Estefano, Vanegas.German");        
         
         System.out.print("Ingrese el número de términos para las series: ");
         int nroTerminos = sc.nextInt();
 
-        // German Vanegas
-        System.out.println("S3: ");
-        SerieTercerEjercicio.generarSerie(nroTerminos);
-        SerieTercerEjercicio.mostrarSerie();
+        // // // German Vanegas
+        // // System.out.println("S3: ");
+        // // SerieTercerEjercicio.generarSerie(nroTerminos);
+        // // SerieTercerEjercicio.mostrarSerie();
         
-        //Francisco Torres
-        SerieNumero4 serieCuatro = new SerieNumero4();
-            System.out.println();
-            System.out.println("S4: ");
-            serieCuatro.TorresSerie4For(nroTerminos);
-            System.out.println();
-            serieCuatro.TorresSerie4while(nroTerminos);
-            System.out.println();
-            serieCuatro.TorresSerie4Dowhile(nroTerminos);
+        // //Francisco Torres
+        // SerieNumero4 serieCuatro = new SerieNumero4();
+        //     System.out.println();
+        //     System.out.println("S4: ");
+        //     serieCuatro.TorresSerie4For(nroTerminos);
+        //     System.out.println();
+        //     serieCuatro.TorresSerie4while(nroTerminos);
+        //     System.out.println();
+        //     serieCuatro.TorresSerie4Dowhile(nroTerminos);
             
-        // Mateo Simbaña
-        System.out.println();
-        System.out.println("S12: ");
-        oSerieNumeros12.showSerieFor(nroTerminos); 
-        System.out.println();
-        oSerieNumeros12.showSerieWhile(nroTerminos);
-        System.out.println();
-        oSerieNumeros12.showSerieDowhile(nroTerminos);
+        // // Mateo Simbaña
+        // System.out.println();
+        // System.out.println("S12: ");
+        // oSerieNumeros12.showSerieFor(nroTerminos); 
+        // System.out.println();
+        // oSerieNumeros12.showSerieWhile(nroTerminos);
+        // System.out.println();
+        // oSerieNumeros12.showSerieDowhile(nroTerminos);
 
         
         sc.close();
